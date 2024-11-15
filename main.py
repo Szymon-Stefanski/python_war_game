52a7e018eccfde1a3f4e76c75cf12e1e250d9fb1
Szymon Stefański <szymonstefanski1@gmail.com>
2024-11-15 18:25:45 +0100

    PROJECT UPDATE - function expansion: Fixed the gold shortage issue when the player wanted to add a soldier to the army without having enough gold, Fixed the issue of removing a soldier from the army.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def create_user(username):
     print("Hello " + username)
 
@@ -8,7 +10,8 @@ def game_interface():
                      "\n2.Sell soldier from army"
                      "\n3.Order maneuvers"
                      "\n4.Check army"
-                     "\n5.Do you want to end turn?: [Y / N / Q]\n")
+                     "\n5.End turn."
+                     "\n6.Capitulation.\n")
 
 
 def army_management():
@@ -56,62 +59,99 @@ def create_game():
         player_decision = game_interface()
 
         match player_decision:
+
             case "1":
                 print("Add soldier to army:")
                 manage = army_management()
                 match manage:
                     case "1":
                         soldier = {"rank": "private", "experience": 1}
-                        print("Private was added")
+                        message = "Private has been added"
                     case "2":
                         soldier = {"rank": "corporal", "experience": 2}
-                        print("Corporal was added")
+                        message = "Corporal has been added"
                     case "3":
                         soldier = {"rank": "captain", "experience": 3}
-                        print("Captain was added")
+                        message = "Captain has been added"
                     case "4":
                         soldier = {"rank": "sergeant", "experience": 4}
-                        print("Sergeant was added")
+                        message = "Sergeant has been added"
                     case "5":
                         soldier = {"rank": "major", "experience": 5}
-                        print("Major was added")
+                        message = "Major has been added"
                     case _:
                         print("Please, choose again.")
                         continue
                 if soldier:
                     if p % 2 == 0:
-                        player_one.append(soldier)
-                        coins_one -= 10 * int(manage)
+                        if coins_one >= 10 * int(manage):
+                            player_one.append(soldier)
+                            print(message)
+                            coins_one -= 10 * int(manage)
+                        else:
+                            print("You don't have enough money!!!")
                     else:
-                        player_two.append(soldier)
-                        coins_two -= 10 * int(manage)
+                        if coins_two >= 10 * int(manage):
+                            player_two.append(soldier)
+                            print(message)
+                            coins_two -= 10 * int(manage)
+                        else:
+                            print("You don't have enough money!!!")
                 continue
+
             case "2":
                 print("Sell soldier from army:")
                 manage = army_management()
                 match manage:
                     case "1":
                         soldier = {"rank": "private", "experience": 1}
+                        message = "Private has been sold"
                     case "2":
                         soldier = {"rank": "corporal", "experience": 2}
+                        message = "Corporal has been sold"
                     case "3":
                         soldier = {"rank": "captain", "experience": 3}
+                        message = "Captain has been sold"
                     case "4":
                         soldier = {"rank": "sergeant", "experience": 4}
+                        message = "Sergeant has been sold"
                     case "5":
                         soldier = {"rank": "major", "experience": 5}
+                        message = "Major has been sold"
                     case _:
                         print("Please, choose again.")
                         continue
                 if soldier:
                     if p % 2 == 0:
-                        player_one.remove(soldier)
+                        if soldier in player_one:
+                            player_one.remove(soldier)
+                            print(message)
+                            coins_one += 10 * int(manage)
+                        else:
+                            print('There isn''t a soldier with that rank in your army!!!')
                     else:
-                        player_two.remove(soldier)
+                        if soldier in player_two:
+                            player_two.remove(soldier)
+                            print(message)
+                            coins_two += 10 * int(manage)
+                        else:
+                            print('There isn''t a soldier with that rank in your army!!!')
                 continue
+
             case "3":
                 print("Order maneuvers")
+                print(random.randint(1, 4))
+                if soldier:
+                    if p % 2 == 0:
+                        for soldier in player_one:
+                            player_one.update()
+                        coins_one -= 50
+                    else:
+                        for soldier in player_two:
+                            player_two.update()
+                        coins_two -= 50
                 continue
+
             case "4":
                 print("Check army")
                 if p % 2 == 0:
@@ -121,17 +161,14 @@ def create_game():
                     for soldier in player_two:
                         print(f"Rank: {soldier['rank']}, Experience: {soldier['experience']}")
                 continue
-            case "Y" | "y":
+            case "5":
                 p = 1 - p
                 if p == 0:
                     i += 1
                     coins_one += 10
                     coins_two += 10
                 continue
-            case "N" | "n":
-                print("You can make another choice.")
-                continue
-            case "Q" | "q":
+            case "6":
                 print(f"{players[p]} surrenders! {players[1 - p]} wins!")
                 surrender = True
                 break
