5025e8b348977cebd89bf7faf65c4c9dccb5641e
Szymon Stefański <szymonstefanski1@gmail.com>
2024-12-01 21:11:42 +0100

    PROJECT UPDATE - added test_game_interface to test game_interface() method.

--- tests_game.py
+++ tests_game.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import patch
 from game import Game
 
 class TestsGame:
@@ -8,3 +9,19 @@ class TestsGame:
         username = 'Example'
         result = game.create_user(username)
         assert result == username, f"Expected {username}, but got {result}"
+
+    @pytest.mark.parametrize("user_input, expected", [
+        ('1', '1'),  # Add soldier
+        ('2', '2'),  # Sell soldier
+        ('3', '3'),  # Promote soldier
+        ('4', '4'),  # Check army
+        ('5', '5'),  # Attack
+        ('6', '6'),  # End turn
+        ('7', '7')  # Capitulation
+    ])
+
+    def test_game_interface(self, user_input, expected):
+        with patch('builtins.input', return_value=user_input):
+            game = Game()
+            result = game.game_interface()
+            assert result == expected, f"Expected {expected}, but got {result}"
